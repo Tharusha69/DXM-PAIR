c6fd9d280b18f685f45f2a2cb62f3b7deac4a698
Tharusha69 <150590598+Tharusha69@users.noreply.github.com>
2024-11-18 16:53:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/code', code);
 
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {

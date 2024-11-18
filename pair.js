e84f66db22b79e6bfda3a76247f3a35869f54fc1
Tharusha69 <150590598+Tharusha69@users.noreply.github.com>
2024-11-18 19:10:52 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,104 +1,93 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
 const express = require('express');
 const fs = require('fs');
-const { exec } = require("child_process");
+const { upload } = require('./mega')
+var auth_path = './sessions/'
 let router = express.Router()
 const pino = require("pino");
 const {
-default: makeWASocket,
-useMultiFileAuthState,
-fetchLatestBaileysVersion,
-DisconnectReason,
-downloadContentFromMessage,
-jidNormalizedUser,
-Browsers,
-delay,
-prepareWAMessageMedia,
-generateWAMessageFromContent,
-getContentType,
-getDevice,
-proto
-} = require('@whiskeysockets/baileys')
-const { upload } = require('./mega');
+    default: Maher_Zubair,
+    useMultiFileAuthState,
+    delay,
+    jidNormalizedUser,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("@whiskeysockets/baileys");
 
-function removeFile(FilePath) {
-    if (!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true });
-}
-
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
 router.get('/', async (req, res) => {
+    const id = makeid();
     let num = req.query.number;
-    async function PrabathPair() {
-        const { state, saveCreds } = await useMultiFileAuthState(`./session`);
-        try {
-            let PrabathPairWeb = makeWASocket({
+        async function SIGMA_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./sessions')
+     try {
+            let Pair_Code_By_Maher_Zubair = Maher_Zubair({
                 auth: {
                     creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
                 },
                 printQRInTerminal: false,
-                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-                browser: Browsers.macOS("Safari"),
-            });
-
-            if (!PrabathPairWeb.authState.creds.registered) {
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Ubuntu", "Chrome", "20.0.04"],
+             });
+             if(!Pair_Code_By_Maher_Zubair.authState.creds.registered) {
                 await delay(1500);
-                num = num.replace(/[^0-9]/g, '');
-                const code = await PrabathPairWeb.requestPairingCode(num);
-                if (!res.headersSent) {
-                    await res.send({ code });
-                }
-            }
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_Maher_Zubair.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data =  fs.readFileSync('./sessions/creds.json')
+                await delay(800);
+                const user_jid = jidNormalizedUser(Pair_Code_By_Maher_Zubair.user.id);
+				   const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${user_jid}.json`);
+                const string_session = mega_url.replace('https://mega.nz/file/', '')
+				   let sessionmsg = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: 'cyber-x@;;;' + string_session });
+	               
+                let SIGMA_MD_TEXT = `* Pair Code Connecting Successfully*
 
-            PrabathPairWeb.ev.on('creds.update', saveCreds);
-            PrabathPairWeb.ev.on("connection.update", async (s) => {
-                const { connection, lastDisconnect } = s;
-                if (connection === "open") {
-                    try {
-                        await delay(10000);
-                        const sessionPrabath = fs.readFileSync('./session/creds.json');
+Don't share this code with any others
 
-                        const auth_path = './session/';
-                        const user_jid = jidNormalizedUser(PrabathPairWeb.user.id);
+https://wa.me/94740952096
 
-                        const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${user_jid}.json`);
+*ᴄᴏʀᴅᴇᴅ ʙʏ ꜱᴀᴅᴇᴇᴘᴀ ᴄʜᴀᴍᴜᴅɪᴛʜ*`
+	 await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:sessionmsg })
 
-                        const string_session = mega_url.replace('https://mega.nz/file/', '');
 
-                        const sid = string_session;
 
-                        const dt = await PrabathPairWeb.sendMessage(user_jid, {
-                            text: sid
-                        });
-
-                    } catch (e) {
-                        exec('pm2 restart prabath');
-                    }
-
-                    await delay(100);
-                    return await removeFile('./session');
-                    process.exit(0);
-                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode !== 401) {
+        await delay(100);
+        await Pair_Code_By_Maher_Zubair.ws.close();
+        return await removeFile('./sessions');
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    PrabathPair();
+                    SIGMA_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
-            exec('pm2 restart prabath-md');
-            console.log("service restarted");
-            PrabathPair();
-            await removeFile('./session');
-            if (!res.headersSent) {
-                await res.send({ code: "Service Unavailable" });
-            }
+            console.log("service restated");
+            await removeFile('./sessions');
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
         }
     }
-    return await PrabathPair();
+    return await SIGMA_MD_PAIR_CODE()
 });
-
-process.on('uncaughtException', function (err) {
-    console.log('Caught exception: ' + err);
-    exec('pm2 restart prabath');
-});
-
-
-module.exports = router;
+module.exports = router

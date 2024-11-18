e53777bb9fe83593b489c8326287730222ab6ce8
Tharusha69 <150590598+Tharusha69@users.noreply.github.com>
2024-11-18 20:45:40 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'alphakingdark@gmail.com',
-    password: 'thinura2003612.',
+    email: 'Tharu2006@',
+    password: 'Tharu2006@',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
